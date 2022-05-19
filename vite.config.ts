0d275023f02722a75d15b36f2171a08eda79a4c8
Andres Garcia <andres.gar@gmail.com>
2022-05-19 17:09:47 -0500

    second

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import {
+  EsLinter,
+  linterPlugin,
+  TypeScriptLinter,
+} from 'vite-plugin-linter'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
+export default defineConfig(configEnv => ({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
     tsconfigPaths(),
+    linterPlugin({
+      build: {
+        includeMode: 'filesInFolder',
+      },
+      include: [
+        './src/**/*.ts',
+        './src/**/*.tsx',
+      ],
+      linters:  [
+        new EsLinter({
+          configEnv,
+          serveOptions: { clearCacheOnStart: true },
+        }),
+        new TypeScriptLinter(),
+      ],
+    }),
   ],
-})
+}))
